0a9ecd0d34cbe803363574913b99e3653a2c1cb4
Stefan Blanke <greenarrow@users.sourceforge.net>
2016-01-28 12:55:54 +0000

    Add pixel wrapper

---
+++ pixel.h
@@ -0,0 +1,14 @@
+#ifndef PIXEL_H
+#define PIXEL_H
+
+#include "common.h"
+
+struct pixel {
+	void *np;
+};
+
+void pixel_init(struct pixel *p, byte pin);
+void pixel_set(struct pixel *p, byte n, struct colour c);
+void pixel_flush(struct pixel *p);
+
+#endif
